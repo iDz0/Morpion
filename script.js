54f5e10c37bf9c7f95fe5c181b73402d39729ab2
iDz0 <46253673+iDz0@users.noreply.github.com>
2019-01-11 00:04:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,64 @@
-var j1 ={nom: "J1", symb: "x"};
-var j2 ={nom: "J2", symb: "y"};
-var joueurs = [j1, j2];
+
+var p1 = {nom: "J1", src: "./img/case_croix.png", symb: " x "};
+var p2 = {nom: "J2", src: "./img/case_rond.png", symb: " o "};
+
+var players = [p1, p2];
+var current_player = p1;
+
 var grille = [
-  [" - ", " - ", " - "],
-  [" - ", " - ", " - "],
-  [" - ", " - ", " - "]
-]
+  " - ", " - ", " - ",
+  " - ", " - ", " - ",
+  " - ", " - ", " - "
+];
 
 
 function aff(){
-  str = grille[0][0] + grille[0][1] + grille[0][2] + "\n" +
-        grille[1][0] + grille[1][1] + grille[1][2] + "\n" +
-        grille[2][0] + grille[2][1] + grille[2][2];
+  str = grille[0] + grille[1] + grille[2] + "\n" +
+        grille[3] + grille[4] + grille[5] + "\n" +
+        grille[6] + grille[7] + grille[8];
   return str;
 }
 
-function initListener(){
-  console.log(document.getElementsByTagName('img').id);
+
+window.onload = function(){
+   imgs = document.getElementsByTagName('img');
+   for(var i = 0; i<9; i++){
+      imgs[i].onclick = function(){
+         if(this.src.slice(-8) == "case.png"){
+            this.src = current_player.src;
+            grille[parseInt(this.id, 10)] = current_player.symb;
+            game();
+         }
+      }
+   }
+}
+
+function game(){
+   if(verif()){
+      win();
+      return;
+   }
+   else if(!grille.includes(" - ")){
+      nulle();
+      return;
+   }
+
+   if(current_player == p1){
+      current_player = p2;
+   }
+   else{
+      current_player = p1;
+   }
+         
+}
+
+function nulle(){
+   console.log('Match nulle');
+}
+
+function verif(){
+   return false;
 }
 
 
 
-initListener();
-
-// while(true){
-//   for(j in joueurs){
-//   }
-// }
